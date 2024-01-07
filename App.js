21574c69e3f1d317e6dd9bc4decc31e2a0c612d2
leventozz <leventufukozturk@gmail.com>
2024-01-07 22:59:16 +0300

    create and edit icons

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from './context/BlogContext';
 import IndexScreen from './screens/IndexScreen';
 import CreateScreen from './screens/CreateScreen';
-import { Provider } from './context/BlogContext';
 import BlogScreen from './screens/BlogScreen';
+import EditScreen from './screens/EditScreen';
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,9 +18,22 @@ export default function App() {
     <Provider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerTitle: 'Blog App' }}>
-          <Stack.Screen name="Index" component={IndexScreen} />
+          <Stack.Screen name="Index" component={IndexScreen} options={({ navigation }) => ({
+            headerRight: () => (
+              <TouchableOpacity onPress={()=> navigation.navigate('Create')}>
+                <AntDesign name="plus" size={24} color="black" />
+              </TouchableOpacity>
+            )
+          })} />
           <Stack.Screen name="Create" component={CreateScreen} />
-          <Stack.Screen name="Blog" component={BlogScreen} />
+          <Stack.Screen name="Blog" component={BlogScreen} options={({ navigation }) => ({
+            headerRight: () => (
+              <TouchableOpacity onPress={()=> navigation.navigate('Edit')}>
+                <Feather name="edit" size={24} color="black" />
+              </TouchableOpacity>
+            )
+          })} />
+          <Stack.Screen name="Edit" component={EditScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
