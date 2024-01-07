d54a5422034cb9ee6c10fe3fdbfbd3b4fdb10997
leventozz <leventufukozturk@gmail.com>
2024-01-07 19:39:44 +0300

    added context

--- App.js
+++ App.js
@@ -4,17 +4,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IndexScreen from './screens/IndexScreen';
 import CreateScreen from './screens/CreateScreen';
+import { BlogProvider } from './context/BlogContext';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerTitle:'Blog App'}}>
-        <Stack.Screen name="Index" component={IndexScreen} />
-        <Stack.Screen name="Create" component={CreateScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerTitle: 'Blog App' }}>
+          <Stack.Screen name="Index" component={IndexScreen} />
+          <Stack.Screen name="Create" component={CreateScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import BlogContext from '../context/BlogContext'
 
 export default function IndexScreen() {
+  const blogPosts = useContext(BlogContext);
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>HomeScreen </Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({item}) => {
+          return <Text>
+            "{item.title}"
+          </Text>
+        }}
+      />
     </View>
   )
 }
