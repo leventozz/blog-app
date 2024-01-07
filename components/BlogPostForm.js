4e32baeab45bb7615fe70b6a037048eb5c339248
leventozz <leventufukozturk@gmail.com>
2024-01-07 23:20:35 +0300

    managed css

--- components/BlogPostForm.js
+++ components/BlogPostForm.js
@@ -5,22 +5,59 @@ export default function BlogPostForm() {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     return (
-        <View>
-            <Text>Title:</Text>
+        <View style={styles.container}>
+            <Text style={styles.label}>Title:</Text>
             <TextInput
+                style={styles.input}
                 value={title}
                 onChangeText={(text) => setTitle(text)}
             />
-            <Text>Tell us your story:</Text>
+            <Text style={styles.label}>Tell us your story:</Text>
             <TextInput
+                style={styles.input}
                 value={content}
                 onChangeText={(text) => setContent(text)}
+                multiline
+                numberOfLines={8}
             />
-            <TouchableOpacity>
-                <Text>Save</Text>
+            <TouchableOpacity style={styles.buttonMain}>
+                <View style={styles.buttonView}>
+                    <Text style={styles.buttonText}>Save</Text>
+                </View>
             </TouchableOpacity>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10,
+    },
+    label: {
+        fontSize: 25,
+        marginLeft: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 5,
+        margin: 10,
+        marginBottom: 15,
+        padding: 5,
+        fontSize: 18,
+        textAlignVertical: 'top',
+    },
+    buttonMain: {
+        padding: 20
+    },
+    buttonView: {
+        backgroundColor: 'green',
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+    }
+})
