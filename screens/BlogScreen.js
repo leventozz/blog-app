91197f19b984a8dce1b09b1095c339a9e57707e9
leventozz <leventufukozturk@gmail.com>
2024-01-08 12:24:24 +0300

    blog screen css

--- screens/BlogScreen.js
+++ screens/BlogScreen.js
@@ -4,13 +4,40 @@ import { Context } from '../context/BlogContext'
 
 export default function BlogScreen({ route }) {
   const { state } = useContext(Context)
-  const blogPost = state.find((blogPost)=> blogPost.id === route.params.id)
+  const blogPost = state.find((blogPost) => blogPost.id === route.params.id)
   return (
-    <View>
-      <Text>{blogPost.title}</Text>
-      <Text>{blogPost.content}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>{blogPost.title}</Text>
+      </View>
+      <View style={styles.divider} />
+      <View>
+        <Text style={styles.content}>{blogPost.content}</Text>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainContainer: {
+    padding: 10,
+  },
+  container: {
+    marginBottom:10,
+  },
+  title: {
+    fontSize: 30,
+    marginLeft: 10,
+    fontWeight:'bold'
+  },
+  content: {
+    fontSize: 18,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: 'black', 
+    marginVertical: 8,
+  }
+})
